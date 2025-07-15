d3e69fc89f48540ba2cf8e2c9c565715de8b3aa2
Anujofficialwork0214 <anuj.vishwakarma@quadbtech.com>
2025-07-15 16:14:14 +0530

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,6 @@ const instrumentSans = Instrument_Sans({
   subsets: ["latin"],
 });
 
-
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
 //   subsets: ["latin"],
@@ -21,20 +20,21 @@ const instrumentSans = Instrument_Sans({
 //   subsets: ["latin"],
 // });
 
-
 // Define the metadata here
 export const metadata = {
   title: "Nek Punjabi Estate ",
-  description: "Nek Punjabi Estate simplifies real estate in Punjab, offering expert guidance and services for buying, selling, and investing in property.",
+  description:
+    "Nek Punjabi Estate simplifies real estate in Punjab, offering expert guidance and services for buying, selling, and investing in property.",
   icons: {
-    icon: "/blacklogo.png", 
+    icon: "/blacklogo.png",
   },
   openGraph: {
     title: "Nek Punjabi Estate | Trusted Real Estate Services in Punjab",
-    description: "Explore expert real estate services with Nek Punjabi Estate. Helping you find the best real estate deals in Punjab.",
+    description:
+      "Explore expert real estate services with Nek Punjabi Estate. Helping you find the best real estate deals in Punjab.",
     images: [
       {
-        url: "/blacklogo.png", 
+        url: "/blacklogo.png",
         width: 1200,
         height: 630,
         alt: "Nek Punjabi Estate Banner",
@@ -43,10 +43,11 @@ export const metadata = {
     type: "website",
   },
   twitter: {
-    card: "summary_large_image", 
+    card: "summary_large_image",
     title: "Nek Punjabi Estate | Trusted Real Estate Services in Punjab",
-    description: "Find the best properties with Nek Punjabi Estate. Expert services for buying, selling, and investing in real estate in Punjab.",
-    images: ["/blacklogo.png"],  
+    description:
+      "Find the best properties with Nek Punjabi Estate. Expert services for buying, selling, and investing in real estate in Punjab.",
+    images: ["/blacklogo.png"],
   },
 };
 
@@ -56,7 +57,7 @@ export default function RootLayout({ children }) {
       <head>
         {/* Optional: You can add a meta tag or other metadata here if necessary */}
       </head>
-    <body className={`${instrumentSans.variable}  antialiased`}>
+      <body className={`${instrumentSans.variable}  antialiased`}>
         <div className="container max-w-[1920px] overflow-x-hidden mx-auto">
           <Navbar />
           {children}
