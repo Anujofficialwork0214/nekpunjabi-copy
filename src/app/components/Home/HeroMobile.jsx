ee00171f965f9876d323295a32e15338170037c2
ShubhamQuadb <shubham.verma@quadbtech.com>
2025-07-15 13:25:36 +0530

    fix sizes and wieghts

--- src/app/components/Home/HeroMobile.jsx
+++ src/app/components/Home/HeroMobile.jsx
@@ -250,7 +250,7 @@ const HeroMobile = () => {
 
         <button
           type="submit"
-          className="w-full py-3  rounded-lg bg-white text-[#AECAEA] font-semibold hover:bg-blue-500 hover:text-black transition"
+          className="w-full py-3 !font-[500] rounded-lg bg-white text-[#AECAEA] font-medium hover:bg-blue-500 hover:text-black transition"
           disabled={loading}
         >
           {loading ? "Sending..." : "Send message"}
